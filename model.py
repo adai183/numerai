76bbe03f43d0a7ec6720fed53ad48e363ca03c9a
adai183 <andreas.daiminger@gmail.com>
2017-02-27 18:09:00 +0100

    refactor

--- model.py
+++ model.py
@@ -1,10 +1,20 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers import BatchNormalization
-from keras.optimizers import Adam
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
+from sklearn.preprocessing import StandardScaler
+
+
+# Set seed for reproducibility
+seed = 0
+np.random.seed(seed)
 
 
 # from keras.models import Sequential
@@ -12,7 +22,6 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
 # from keras.layers import BatchNormalization
 # from keras.optimizers import Adam
 # from keras.regularizers import l2
-# from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
 
 
 # def process(x, y):
@@ -49,88 +58,67 @@ def main():
     # Load the data from the CSV files
     training_data = pd.read_csv(
         'numerai_datasets/numerai_training_data.csv', header=0)
-    prediction_data = pd.read_csv(
-        'numerai_datasets/numerai_tournament_data.csv', header=0)
 
     # Transform the loaded CSV data into numpy arrays
     Y = training_data['target'].as_matrix()
+
+    # encode class values as integers
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    Y = encoder.transform(Y)
+
     X = training_data.drop('target', axis=1).as_matrix()
-    t_id = prediction_data['t_id'].as_matrix()
-    x_prediction = prediction_data.drop('t_id', axis=1).as_matrix()
+    scaler = StandardScaler().fit(X)
+    X = scaler.transform(X)
+    # t_id = prediction_data['t_id'].as_matrix()
+    # x_prediction = prediction_data.drop('t_id', axis=1).as_matrix()
 
-    print(X.shape)
+    # larger model
+    def Model():
+        # create model
+        model = Sequential()
+        model.add(Dense(500, input_dim=50, init='normal', activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(250, init='normal', activation='relu'))
+        model.add(Dropout(.2))
+        model.add(Dense(150, init='normal', activation='relu'))
+        model.add(Dropout(.2))
+        model.add(Dense(100, init='normal', activation='relu'))
+        model.add(Dropout(.2))
+        model.add(Dense(50, init='normal', activation='relu'))
+        model.add(Dropout(.2))
+        model.add(Dense(10, init='normal', activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(1, init='normal', activation='sigmoid'))
+        # Compile model
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam', metrics=['accuracy'])
+        return model
 
-    model = Sequential()
+    # estimators = []
+    # estimators.append(('standardize', StandardScaler()))
+    # estimators.append(('mlp', KerasClassifier(
+    #     build_fn=create_larger,
+    #     nb_epoch=100,
+    #     batch_size=2000,
+    #     verbose=1)))
 
-    # 0. Dense 2128
-    model.add(Dense(16000, input_dim=50))
-    model.add(Activation('elu'))
+    # pipeline = Pipeline(estimators)
+    # kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+    # results = cross_val_score(pipeline, X, Y, cv=kfold)
+    # print(results)
+    # print("Performance: %.2f%% (%.2f%%)" %
+    #       (results.mean() * 100, results.std() * 100))
 
-    # 0. Dense 2128
-    model.add(Dense(8000))
-    model.add(Activation('elu'))
-
-    # 0. Dense 2128
-    model.add(Dense(4000))
-    model.add(Activation('elu'))
-
-    # 1. Dense 1064
-    model.add(Dense(2000, init='uniform'))
-    model.add(Activation('elu'))
-
-    # 2. Dense 532
-    model.add(Dense(1000, init='uniform'))
-    model.add(Activation('elu'))
-
-    # 3. Dense 256
-    model.add(Dense(500, init='uniform'))
-    model.add(Activation('elu'))
-
-    # 4. Dense 128
-    model.add(Dense(250, init='uniform'))
-    model.add(Activation('elu'))
-
-    # 5. Dense 64
-    model.add(Dense(100, init='uniform'))
-    model.add(Activation('elu'))
-
-    # 6. Dense 32
-    model.add(Dense(50, init='uniform'))
-    model.add(Activation('elu'))
-
-    # 7. Dense 16
-    model.add(Dense(10, init='uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-    model.add(Dropout(.5))
-
-    # 8. Dense 8
-    model.add(Dense(6, init='uniform'))
-    model.add(Activation('elu'))
-    model.add(Dropout(.5))
-
-    # 9. Output
-    model.add(Dense(1))
-
-    # Configures the learning process and metrics
-    model.compile(optimizer=Adam(lr=0.0001),
-                  loss='mean_squared_logarithmic_error', metrics=['accuracy'])
-
-    # Train the model
-    # History is a record of training loss and metrics
+    model = Model()
     model.fit(
         X,
         Y,
-        batch_size=128,
-        nb_epoch=100,
+        batch_size=1000,
+        nb_epoch=150,
         shuffle=True,
         validation_split=0.2,
-        callbacks=[EarlyStopping(monitor='val_acc',
-                                 min_delta=0.001,
-                                 patience=10,
-                                 verbose=2,
-                                 mode='auto'),
-                   ModelCheckpoint('model.h5',
+        callbacks=[ModelCheckpoint('model.h5',
                                    monitor='val_acc',
                                    verbose=1,
                                    save_best_only=True,
